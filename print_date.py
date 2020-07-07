f622e68cd89101ac4ef5cdf3d0b04687254fa8d9
cailongping <cailp_hj@163.com>
2020-07-07 19:50:59 +0800

    add a new program to print date second time

--- print_date.py
+++ print_date.py
@@ -2,8 +2,5 @@ import calendar
 
 c=calendar.TextCalendar(calendar.MONDAY)
 dates=c.prmonth(2020,7)
-print(dates)
-
-print('*'*100)
-dates_=c.promonth(2020,8)
-print(dates_)
+print('='*100)
+dates_=c.prmonth(2020,8)
